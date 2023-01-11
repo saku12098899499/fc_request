d79aae00b78f2caaac1547a7a754833680cf9a90
saku12098899499 <90257895+saku12098899499@users.noreply.github.com>
2023-01-11 18:58:46 +0900

    Delete FC_registry directory

--- FC_registry/fc_request/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Wallet,Category,Rating,Request,Response
-
-admin.site.register(Wallet)   # adminサイトにWalletモデルを追加
-admin.site.register(Category) # adminサイトにCategoryモデルを追加
-admin.site.register(Rating)  # adminサイトにRatingモデルを追加
-admin.site.register(Request)  # adminサイトにRequestモデルを追加
-admin.site.register(Response) # adminサイトにResponseモデルを追加
